54a02ceb501b38d58f0cff66a4b325de9b75b852
Max Hjärpe <max.hjarpe.syne21lin@tucsweden.se>
2021-10-14 13:34:47 +0200

    tagit bort deklarering av double längst upp och ändrat kommentarer

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,17 +7,18 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Title = "MINIRÄKNARE";
-            double num1, num2, resultat; 
+            Console.Title = "MINIRÄKNARE"; 
             char[] operatorer = { '*', '/', '-', '+' }; //Operatorerna som vi letar efter i användarens inmatning
             List<string> svar = new List<string>(); //Listan som vi sparar alla uträkningar i
 
             Console.WriteLine("Hej och välkommen till miniräknaren i C#\nVar god skriv in ett tal, " +
                 "följt av en operator \"+\", \"-\", \"*\" eller \"/\", följt av ett till tal." +
                 "\nNär du vill avsluta, skriver du \"MARCUS\""); //OBS att endast en operator kan användas per uträkning.
-                                                                 //Alltså funkar inte 1+1+1. Programmet kommer att räkna allt från första operatorn
-                                                                 //till slutet av strängen som ett tal och misslyckas att konvertera till double
-                                                                 //se rad 40 och 48 för konverteringen.
+                                                                 //Exempelvis funkar inte 1+1+1. Programmet kommer att räkna allt från första operatorn
+                                                                 //till slutet av strängen som ett tal och misslyckas att konvertera till double. 
+                                                                 //Felmeddelandet blir också "felformulerat" om man använder fler operatorer. Felet uppstår på grund av
+                                                                 //mitt sätt att ta reda på de två olika talen där jag letar efter första operatorn som förekommer
+                                                                 //och plockar ut det som finns på vardera sida av operatorn. Se raderna 40-42 och 44-49. 
             
             while (true)
             {
@@ -40,26 +41,26 @@ namespace Calculator
                     string andraTalet = inmatning.Substring(vartOperator + 1); //Plocka ut andra talet på samma sätt som tidigare, OBS blir fel om fler operatorer används
                     string vilkenOperator = inmatning.Substring(vartOperator, 1); //Plocka ut VILKEN operator som matats in 
                     
-                    if (!double.TryParse(forstaTalet, out num1)) //Kolla strängen efter ett tal och gör om till double om det går, annars får användaren göra om
+                    if (!double.TryParse(forstaTalet, out double num1)) //Kolla strängen efter ett tal och gör om till double om det går, annars får användaren göra om
                     {
                         Console.WriteLine("Vafalls? " + "\"" + forstaTalet + "\"" + " är inget tal... försök igen!");
                         continue;
                     }
-                    if (!double.TryParse(andraTalet, out num2)) //Kolla strängen efter ett tal och gör om till double om det går, annars får användaren göra om 
+                    if (!double.TryParse(andraTalet, out double num2)) //Kolla strängen efter ett tal och gör om till double om det går, annars får användaren göra om 
                     {
                         Console.WriteLine("Nu tabbade du dig " + "\"" + andraTalet + "\"" + " är inget tal... försök igen!"); 
                         continue;
                     }
-                    if (forstaTalet == "0" || andraTalet == "0" && vilkenOperator == "/") //Ser till att användaren inte dividerar med 0
+                    if (andraTalet == "0" && vilkenOperator == "/") //Ser till att användaren inte dividerar med 0
                     {
-                        Console.WriteLine("Du får inte dividera med 0! Försök igen");
+                        Console.WriteLine("Du får inte dividera med 0! Försök igen!");
                         continue;
                     }
                     
                     switch (vilkenOperator) //Gör uträkningen, matar ut svaret och sparar det i listan
                     {
                         case "+":
-                            resultat = (num1 + num2);
+                            double resultat = (num1 + num2);
                             Console.WriteLine(num1 + "+" + num2 + " = " + resultat + "\n"); 
                             svar.Add(num1 + "+" + num2 + " = " + resultat); 
                             break;
